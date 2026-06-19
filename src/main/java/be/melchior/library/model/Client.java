6842bfa0db2cd85f508b0c5109aa1e03ab4c8ce4
Melchior Dengis <mel.dengis@gmail.com>
2026-06-19 18:54:30 +0100

    Added a value checker for the constructor

--- src/main/java/be/melchior/library/model/Client.java
+++ src/main/java/be/melchior/library/model/Client.java
@@ -10,21 +10,43 @@ public class Client {
 
 
     public Client(String firstName, String lastName, String phoneNumber){
+        validate(firstName, "first name");
+        validate(lastName, "last name");
+        validate(phoneNumber, "phone number");
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
     }
 
-    public String getfirstName(){
+    public String getFirstName(){
         return this.firstName;
     }
 
-    public String getLastname(){
+    public String getLastName(){
         return this.lastName;
     }
 
     public String getPhoneNumber(){
         return this.phoneNumber;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void validate(String value, String fieldName){
+        if(value == null || value.isEmpty()){
+            throw new IllegalArgumentException(fieldName + " cannot be empty");
+        }
     }
 
 
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,10 +1,49 @@
+import be.melchior.library.model.Client;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClientTest {
 
+    private Client client;
+
+    @BeforeEach
+    public void createClient(){
+        client = new Client("Melchior", "Dengis", "0493911842");
+    }
+
 
     @Test
-    public void clientShouldHaveAName(){
-        assertThat
+    public void clientShouldHaveAFirstName(){
+        assertEquals("Melchior", client.getFirstName());
+    }
+
+    @Test
+    public void clientShouldHaveALastName(){
+        assertEquals("Dengis", client.getLastName());
+    }
+
+    @Test
+    public void clientShouldHaveAPhoneNumber(){
+        assertEquals("0493911842", client.getPhoneNumber());
+    }
+
+    @Test
+    public void clientFirstNameShouldChange(){
+        client.setFirstName("Thomas");
+        assertEquals("Thomas", client.getFirstName());
+    }
+
+    @Test
+    public void clientLastNameShouldChange(){
+        client.setLastName("Dangis");
+        assertEquals("Dangis", client.getLastName());
+    }
+
+    @Test
+    public void clientPhoneNumberShouldChange(){
+        client.setPhoneNumber("0492981850");
+        assertEquals("0492981850", client.getPhoneNumber());
     }
 }
