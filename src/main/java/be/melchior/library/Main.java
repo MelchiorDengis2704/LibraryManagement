a3b0f86b057d4080a58a9b103b70fcd09942758a
Melchior Dengis <mel.dengis@gmail.com>
2026-06-19 17:06:11 +0100

    Initializing mvn safety checker dependency

--- src/main/java/be/melchior/library/Main.java
+++ src/main/java/be/melchior/library/Main.java
@@ -5,7 +5,7 @@ import be.melchior.library.model.Client;
 
 public class Main {
     public static void main(String[] args) {
-        Client c = new Client();
+        Client c = new Client("Melchior", "Dengis", "0493911842");
         System.out.println(c);
     }
 }
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,6 @@
+public class ClientTest {
+
+
+    @Test
+
+}
