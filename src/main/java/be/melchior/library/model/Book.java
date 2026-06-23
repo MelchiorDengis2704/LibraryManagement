aabf13a65e172ace258d683619c7b88c3077fa42
Melchior Dengis <mel.dengis@gmail.com>
2026-06-23 19:58:14 +0100

    Maked the book class

---
+++ src/main/java/be/melchior/library/model/Book.java
@@ -0,0 +1,26 @@
+package be.melchior.library.model;
+
+public class Book {
+
+    private String title;
+    private Category category;
+
+    public Book(String title, Category category){
+        if(title == null || title.isEmpty()){
+            throw new IllegalArgumentException("The book can't have a empty title ");
+        }
+        this.title = title;
+        this.category = category;
+    }
+
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public Category getCategory(){
+        return this.category;
+    }
+
+
+}
