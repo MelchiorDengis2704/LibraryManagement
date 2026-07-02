8cd290f9fceab8ba38442b2fa7e0efa1c7d9a181
Melchior Dengis <mel.dengis@gmail.com>
2026-07-02 12:33:39 +0100

    Adding the Library Service for applying business rules

---
+++ src/main/java/be/melchior/library/service/LibraryService.java
@@ -0,0 +1,16 @@
+package be.melchior.library.service;
+
+import be.melchior.library.model.Book;
+import be.melchior.library.model.Loan;
+
+public class LibraryService {
+
+
+
+
+
+
+    public void loanBook(Loan loan, Book book){
+
+    }
+}
